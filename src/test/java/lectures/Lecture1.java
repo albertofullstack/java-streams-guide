9ba59ff56063c56932e6041cce7c5d1ff039b3ee
albertofullstack <alberto.fullstack@gmail.com>
2023-11-05 19:54:13 +0100

    Implements first exercise.

--- src/test/java/lectures/Lecture1.java
+++ src/test/java/lectures/Lecture1.java
@@ -6,6 +6,7 @@ import beans.Person;
 
 import com.google.common.collect.ImmutableList;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,6 +22,16 @@ public class Lecture1 {
     List<Person> people = MockData.getPeople();
     // 1. Find people aged less or equal 18
     // 2. Then change implementation to find first 10 people
+    List<Person> youngPeople = new ArrayList<>();
+    System.out.println("Young people");
+    int count = 0;
+    for (Person person : people) {
+      if (person.getAge() <= 18 && count < 10) {
+        youngPeople.add(person);
+        System.out.println(person);
+        count++;
+      }
+    }
 
   }
 
