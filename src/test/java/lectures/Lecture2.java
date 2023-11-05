b4468636ba5b42e05dae3626ec7d9f6b0913ac07
albertofullstack <alberto.fullstack@gmail.com>
2023-11-05 20:28:15 +0100

    Add IntStream example.

--- src/test/java/lectures/Lecture2.java
+++ src/test/java/lectures/Lecture2.java
@@ -10,6 +10,18 @@ public class Lecture2 {
 
   @Test
   public void range() throws Exception {
+    System.out.println("for i");
+    for (int i = 0; i <= 10; i++) {
+      System.out.println(i);
+    }
+
+    System.out.println("Exclusive");
+    IntStream.range(0, 10)
+        .forEach(System.out::println);
+
+    System.out.println("Inclusive");
+    IntStream.rangeClosed(0, 10)
+        .forEach(System.out::println);
 
   }
 
