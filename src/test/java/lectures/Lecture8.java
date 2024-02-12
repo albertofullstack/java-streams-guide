aa5d76d4c12f8facf92e8ea4d28385cb68038be9
amoranteb <amoranteb@minsait.com>
2024-02-12 21:04:50 +0100

    Implements simpleGrouping.

--- src/test/java/lectures/Lecture8.java
+++ src/test/java/lectures/Lecture8.java
@@ -16,7 +16,14 @@ public class Lecture8 {
 
   @Test
   public void simpleGrouping() throws Exception {
+    Map<String, List<Car>> carsByMake = MockData.getCars()
+        .stream()
+        .collect(Collectors.groupingBy(Car::getMake));
 
+    carsByMake.forEach((make, cars) -> {
+      System.out.println(make);
+      cars.forEach(System.out::println);
+    });
   }
 
   @Test
